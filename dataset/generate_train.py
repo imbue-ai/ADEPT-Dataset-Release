2c14cd8c4bd49d42d970cdf5f8393bc522041d60
Bartosz Wróblewski <bawr@hszm.pl>
2020-10-07 17:19:55 -0700

    The unfuckening, 5/N. GPUs not working really.

--- dataset/generate_train.py
+++ dataset/generate_train.py
@@ -170,7 +170,39 @@ def main(case_id, args):
             break
 
 
+def enable_gpus(device_type, use_cpus=False):
+    import bpy
+    preferences = bpy.context.preferences
+    cycles_preferences = preferences.addons["cycles"].preferences
+    cuda_devices, opencl_devices = cycles_preferences.get_devices()
+
+    if device_type == "CUDA":
+        devices = cuda_devices
+    elif device_type == "OPENCL":
+        devices = opencl_devices
+    else:
+        raise RuntimeError("Unsupported device type")
+
+    activated_gpus = []
+
+    for device in devices:
+        if device.type == "CPU":
+            device.use = use_cpus
+        else:
+            device.use = True
+            activated_gpus.append(device.name)
+
+    cycles_preferences.compute_device_type = device_type
+    bpy.context.scene.cycles.device = "GPU"
+
+    print(activated_gpus)
+
+    return activated_gpus
+
+
 if __name__ == '__main__':
+#   TODO: enable CUDA in a way that actually uses it
+    enable_gpus("CUDA")
     args = parse_args()
     catch_abort()
     worker_args = []
@@ -179,5 +211,11 @@ if __name__ == '__main__':
     for i in range(args.start_index, args.end, args.stride):
         worker_args.append((i, args))
 
-    with Pool(2) as p:
-        p.starmap(main, worker_args)
+    if False:
+        with Pool(2) as p:
+            p.starmap(main, worker_args)
+    else:
+        for wa in worker_args:
+            print("WORKER DOES")
+            main(*wa)
+            print("WORKER DONE")
